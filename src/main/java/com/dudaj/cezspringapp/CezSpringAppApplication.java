3f9907bbbd18794d3e899a4913db23872953528d
Jakub Duda <121130603+Dudu7352@users.noreply.github.com>
2026-06-23 22:31:20 +0200

    initial commit

---
+++ src/main/java/com/dudaj/cezspringapp/CezSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.dudaj.cezspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CezSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CezSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dudaj/cezspringapp/CezSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudaj.cezspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CezSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
