5dd2083fa871e796a377efad19e34fa83e36c3a9
Jakub Duda <121130603+Dudu7352@users.noreply.github.com>
2026-06-24 14:01:02 +0200

    add documentation

--- src/main/java/com/dudaj/cezspringapp/service/PatientService.java
+++ src/main/java/com/dudaj/cezspringapp/service/PatientService.java
@@ -20,19 +20,41 @@ public class PatientService {
     @Autowired
     private final ReceiptRepository receiptRepository;
 
+    /**
+     * Retrieves a patient with the specified PESEL number.
+     *
+     * @param pesel patient's PESEL
+     * @return On success, a patient is returned wrapped inside Optional container. If none were found, an empty list is returned
+     */
     Optional<PatientDto> getPatient(String pesel) {
         return patientRepository.findByPesel(pesel).map(PatientMapper::toDto);
     }
 
+    /**
+     * Returns all patients
+     *
+     * @return a list of all patients
+     */
     List<PatientDto> getAllPatients() {
         return patientRepository.findAll().stream().map(PatientMapper::toDto).toList();
     }
 
+    /**
+     * Adds a new patient.
+     *
+     * @param newPatientDto new patient
+     * @return On success, added patient is returned wrapped inside Optional container. On failure, an empty optional is returned
+     */
     Optional<PatientDto> addPatient(PatientDto newPatientDto) {
         Patient newPatient = PatientMapper.fromDto(newPatientDto);
         return patientRepository.save(newPatient).map(PatientMapper::toDto);
     }
 
+    /**
+     * Removes a patient and every receipt associated with his PESEL. This action is idempotent.
+     *
+     * @param pesel patient's PESEL
+     */
     void removePatient(String pesel) {
         patientRepository.deleteByPesel(pesel);
         receiptRepository.deleteByPesel(pesel);
