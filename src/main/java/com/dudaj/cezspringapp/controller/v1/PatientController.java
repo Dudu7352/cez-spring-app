a2da1abb76cc35739eb8f378a2f379739f5dbc9e
Jakub Duda <121130603+Dudu7352@users.noreply.github.com>
2026-06-25 23:31:18 +0200

    validate data in controllers

--- src/main/java/com/dudaj/cezspringapp/controller/v1/PatientController.java
+++ src/main/java/com/dudaj/cezspringapp/controller/v1/PatientController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/patients")
 @RequiredArgsConstructor
+@Validated
 public class PatientController {
     @Autowired
     private final PatientService patientService;
--- src/main/java/com/dudaj/cezspringapp/controller/v1/PrescriptionController.java
+++ src/main/java/com/dudaj/cezspringapp/controller/v1/PrescriptionController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/prescriptions")
 @RequiredArgsConstructor
+@Validated
 public class PrescriptionController {
     @Autowired
     private final PrescriptionService prescriptionService;
